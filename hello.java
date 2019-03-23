f2fa8af68ea61a159890d455d1b865ba8949f6b9
Gajanan Birajdar <birajdargajanan0@gmail.com>
2019-03-23 13:43:12 +0530

    first commit

--- hello.java
+++ hello.java
@@ -3,8 +3,12 @@ package GitP;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("hello world");
+		
+		System.out.println("hello git");
+		
+		System.out.println("second commit");
 	}
 
 }
